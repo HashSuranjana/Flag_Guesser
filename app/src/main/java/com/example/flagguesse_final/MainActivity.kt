adb03eaa5a5030c35c64fc6bd57b59b22a8bf8e4
Hasitha Suranjana <hasithasuranjana12@gmail.com>
2024-03-06 11:41:43 +0530

    Initial Update

---
+++ app/src/main/java/com/example/flagguesse_final/MainActivity.kt
@@ -0,0 +1,114 @@
+package com.example.flagguesse_final
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.flagguesse_final.ui.theme.FlagGuessefinalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FlagGuessefinalTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column (modifier= Modifier
+                        .fillMaxSize()
+                        .background(color = Color.LightGray)
+                        .padding(40.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceBetween){
+
+                        Text("Flag Guesser")
+                        MyButtons()
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Mytext(text:String){
+    Text(text = text)
+}
+
+@Composable
+fun MyButtons(){
+    Column (modifier = Modifier
+        .height(150.dp)
+        .width(500.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround){
+
+        Row(modifier = Modifier.width(300.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+            ) {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Guess Country")
+
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Guess-Hints")
+
+            }
+        }
+
+        Row (modifier = Modifier.width(300.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Guess Flag")
+
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Advance-Level")
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FlagGuessefinalTheme {
+        Column (modifier= Modifier
+            .fillMaxSize()
+            .background(color = Color.LightGray)
+            .padding(40.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween){
+
+            Text("Flag Guesser")
+            MyButtons()
+
+        }
+
+    }
+}
