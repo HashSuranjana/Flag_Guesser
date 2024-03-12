798b842996fab203984599e609cd68a6f92806db
Hasitha Suranjana <hasithasuranjana12@gmail.com>
2024-03-12 14:22:23 +0530

    Updated files

--- app/src/main/java/com/example/flagguesse_final/GuessFlagActivity.kt
+++ app/src/main/java/com/example/flagguesse_final/GuessFlagActivity.kt
@@ -7,12 +7,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.contextaware.ContextAware
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -21,9 +25,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.flagguesse_final.ui.theme.FlagGuessefinalTheme
 
 class GuessFlagActivity : ComponentActivity() {
@@ -42,13 +50,14 @@ class GuessFlagActivity : ComponentActivity() {
             }
         }
     }
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        FlagGuessefinalTheme {
-            GuessFlagGame()
 
-        }
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FlagGuessefinalTheme {
+        GuessFlagGame()
+
     }
 }
 
@@ -62,35 +71,47 @@ fun GuessFlagGame() {
     var countryCode = rememberSaveable { Data().countryCodes.random() } //Get a random country code from the CountryCodes
 
     var countryName = rememberSaveable {Data().country_names[countryCode]?: "Unknown"}  // Get the relevant country name for that country Code
-    
+
     var countryflag = rememberSaveable {Data().countryFlags[countryCode]?: R.drawable.ad} // Get the relevant Flag image for that country Code
 
 
     val excludedFlag = flagIds.indexOf(countryflag) // Find the index of the current flag
 
-    val remainingFlags = flagIds.filterIndexed { index, _ -> index != excludedFlag } // Exclude the current flag
+    val remainingFlags = flagIds.filterIndexed { index, _ -> index != excludedFlag } // Exclude the current flag get other flags
 
-    val randomFlags = remember { remainingFlags.shuffled().take(2) } // Randomly select two flags from the remaining list
+    val randomFlags = remember { remainingFlags.shuffled().take(2) } // Randomly select two flags from the other flags
 
 
-
+    val shuffledFlags = remember { (randomFlags + countryflag).shuffled()  } // Add the current flag to the shuffled list
 
     Column (
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally){
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround){
 
-        Text(text = countryName)
+        Text(text = countryName,
+            style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold))
 
-        Image(painter = painterResource(id = countryflag), contentDescription = null)
-
-        Image(painter = painterResource(id = randomFlags[0]), contentDescription = null)
-        Image(painter = painterResource(id = randomFlags[1]), contentDescription = null)
+        // Display the shuffled flags
+        shuffledFlags.forEach { flagResourceId ->
+            Box(modifier = Modifier.width(160.dp).height(130.dp)
+                .background(color= androidx.compose.ui.graphics.Color.Gray),
+                Alignment.Center){
+                Image(
+                    painter = painterResource(id = flagResourceId),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(150.dp) // Predefined width and height for the flags
+                        .padding(4.dp)
+                        .clickable {
+                            
+                        }
+                )
+            }
+        }
     }
-
-    
-
-
 }
+
 
